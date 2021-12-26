d2601173c20908aa0ed1908f5b9d875b51707a83
alsak0de <63293958+alsak0de@users.noreply.github.com>
2021-12-26 21:12:25 +0100

    Update whinreceiver.js

--- whinreceiver.js
+++ whinreceiver.js
@@ -15,7 +15,9 @@ module.exports = function (RED) {
         const topik = "whin/"+node.authconf.token+"/#";
         function suscribe(){}
         mqclient.on("connect", () => {mqclient.subscribe(topik+"/#",{qos:2})});
-        mqclient.on('message', (topic, data) => {msg.payload=data; node.send(msg)})
+        mqclient.on('message', (topic, data) => {
+		var msg = {payload:data};
+		node.send(msg)})
         mqclient.on("error",()=>{node.warn("Error: Whin Receiver not accessible. Please check Internet connection.")});
 	}
 	RED.nodes.registerType("whinreceiver", whinreceiver);
