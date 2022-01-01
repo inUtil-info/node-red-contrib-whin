cc4e315f31696f7508a61733e225d56877b74117
alsak0de <63293958+alsak0de@users.noreply.github.com>
2022-01-01 21:38:42 +0100

    Create whin-receiver.js

---
+++ whin-receiver.js
@@ -0,0 +1,11 @@
+ const fs = require("fs");
+ const path = require("path");
+
+ module.exports = function(RED) {
+     const subflowFile = path.join(__dirname,"whin-receiver.json");
+     const subflowContents = fs.readFileSync(subflowFile);
+     const subflowJSON = JSON.parse(subflowContents);
+     RED.nodes.createNode(config);
+     const authconf = RED.nodes.getNode(config.auth); //hoping authconf will be accessible to the subflow
+     RED.nodes.registerSubflow(subflowJSON);
+ }
