5cf4edba5a7ea53a8ddd5f576aa63e5ee25f83a2
alsak0de <63293958+alsak0de@users.noreply.github.com>
2022-01-04 01:15:26 +0100

    Update whin-receive.js

--- whin-receive.js
+++ whin-receive.js
@@ -2,7 +2,7 @@
  const path = require("path");
 
  module.exports = function(RED) {
-     const subflowFile = path.join(__dirname,"whin-recieve.json");
+     const subflowFile = path.join(__dirname,"whin-receive.json");
      const subflowContents = fs.readFileSync(subflowFile);
      const subflowJSON = JSON.parse(subflowContents);
      RED.nodes.registerSubflow(subflowJSON);
