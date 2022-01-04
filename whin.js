badb634779e8bbfa021f6ac178377fde69c79f4a
alsa-kode <alberto@sagarra-novo.net>
2022-01-04 01:12:17 +0100

    okRevert "Update whin.js"
    This reverts commit 278c951c30294d46f217559bcf9393c33bd11a71.

--- whin.js
+++ whin.js
@@ -1,15 +1,4 @@
-
-
-
 module.exports = function (RED) {
-	const fs = require("fs");
-        const path = require("path");
-	const receiveFile = path.join(__dirname,"whin-recieve.json");
-        const receiveContents = fs.readFileSync(receiveFile);
-        const receiveJSON = JSON.parse(receiveContents);
-	const confirmFile = path.join(__dirname,"whin-confirm.json");
-        const confirmContents = fs.readFileSync(confirmFile);
-        const confirmJSON = JSON.parse(confirmContents);
 	function WhinNode(config) {
 		var https = require('https');
 		RED.nodes.createNode(this, config);
@@ -55,6 +44,4 @@ module.exports = function (RED) {
 	}
 
 	RED.nodes.registerType("whin", WhinNode);
-	RED.nodes.registerSubflow(receiveJSON);
-	RED.nodes.registerSubflow(confirmJSON);
 }
