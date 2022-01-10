40e538c3f54e4d600d16a2b41c12c5ed8069daeb
alsak0de <63293958+alsak0de@users.noreply.github.com>
2022-01-10 01:01:02 +0100

    Delete whin-confirm.js

--- whin-confirm.js
+++
@@ -1,9 +0,0 @@
- const fs = require("fs");
- const path = require("path");
-
- module.exports = function(RED) {
-     const subflowFile = path.join(__dirname,"whin-confirm.json");
-     const subflowContents = fs.readFileSync(subflowFile);
-     const subflowJSON = JSON.parse(subflowContents);
-     RED.nodes.registerSubflow(subflowJSON);
- }
