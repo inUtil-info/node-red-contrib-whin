fc9881af4275d20dfc42dcff1d5befab940d00d9
alsak0de <63293958+alsak0de@users.noreply.github.com>
2022-01-10 01:01:32 +0100

    Delete whin-config.js

--- whin-config.js
+++
@@ -1,9 +0,0 @@
-module.exports = function (RED) {
-    function WhinConfig(n) {
-        RED.nodes.createNode(this, n);
-        this.phone = n.phone;
-        this.token = n.token;
-    }
-
-    RED.nodes.registerType("whin-config", WhinConfig);
-}
